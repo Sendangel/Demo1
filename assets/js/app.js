53ff05b00c4b1c03faa1fe86d752be72c9e00cac
Sendangel <sendangel2016@gmail.com>
2022-08-08 03:47:53 +0300

    add aos.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
 $(function() {
 
+
+    /* Nav Toggle on momile
+    =================================================*/
+
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
+
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+
+        $("body").toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+    });
+
+    $(window).on("resize", function() {
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
+
+
+
+
     let intro = $("#intro");
     let header = $("#header");
     let introH = intro.innerHeight();
@@ -38,6 +62,10 @@ $(function() {
 
         let scrollEl = $(this).data("scroll");
         let scrollElPos = $(scrollEl).offset().top;
+
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('avtive');
+        nav.removeClass('show');
 
         $("html, body").animate({
             scrollTop: scrollElPos - headerH
@@ -131,8 +159,8 @@ $(function() {
 
 
     /* Slick slider 
-    ====================================
-    https://kenwheeler.github.io/slick/ */ 
+    https://kenwheeler.github.io/slick/ 
+    ====================================================== */ 
 
 
     /* Intro Slider */ 
@@ -159,7 +187,8 @@ $(function() {
     });
 
 
-    /* Reviews Slider */
+    /* Reviews Slider 
+    ===============================================*/
     
     let reviewsSlider = $("#reviewsSlider");
 
@@ -172,6 +201,37 @@ $(function() {
         speed: 500
     });
     
+
+    /* Aos.js 
+       https://github.com/michalsnik/aos
+    =====================================================*/ 
+
+    AOS.init({
+    // Global settings:
+    disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 80, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 700, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+
+
 
 });
 
